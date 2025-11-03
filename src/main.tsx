0e15f264a8c7cc864caf081c189b5e2c37d23898
Exist <existqq@gmail.com>
2025-11-03 22:57:56 +0300

    add api integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,17 @@
 import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import Categories from "./pages/Categories";
+import CategoryDetail from "./pages/CategoryDetail";
 import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<></>);
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/categories" element={<Categories />} />
+      <Route path="/categories/:id" element={<CategoryDetail />} />
+    </Routes>
+  </BrowserRouter>
+);
