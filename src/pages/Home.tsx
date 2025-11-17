a03a21c0ef0fc9b688a1b50c4d2a41f8557ef70f
Exist <existqq@gmail.com>
2025-11-17 18:54:36 +0300

    remove button section from start page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,4 @@
-import { Link } from 'react-router-dom';
-import { Container, Button } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import Header from '../components/layout/Header';
 import './Home.css';
 
@@ -14,14 +13,6 @@ function Home() {
             Комплексная платформа для расчета и анализа ИПЦ на основе категорий услуг 
             и их ценовых данных. Отслеживайте инфляцию и оценивайте изменения в стоимости жизни.
           </p>
-          <div className="hero-buttons d-flex gap-3">
-            <Button as={Link} to="/categories" variant="primary" size="lg" className="hero-button">
-              Категории
-            </Button>
-            <Button variant="outline-secondary" size="lg" disabled className="hero-button">
-              Расчеты
-            </Button>
-          </div>
         </Container>
       </section>
     </div>
