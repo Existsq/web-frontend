e98fb7a59c2504a083d801c8e9c724350c53e278
Exist <existqq@gmail.com>
2025-11-18 14:00:49 +0300

    pwa configuration update

--- vite.config.ts
+++ vite.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
         theme_color: "#ffffff",
         background_color: "#ffffff",
         display: "standalone",
-        start_url: "index.html",
+        start_url: "categories",
         icons: [
           {
             src: "/pwa-192x192.png",
